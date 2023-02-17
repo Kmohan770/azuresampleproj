16230fcf43efc2aaaca2630a327f0a835c74f477
Kmohan770 <31613103+Kmohan770@users.noreply.github.com>
2023-02-17 15:25:42 +0530

    Update AzuresampleappApplication.java

--- src/main/java/com/local/azuresampleapp/AzuresampleappApplication.java
+++ src/main/java/com/local/azuresampleapp/AzuresampleappApplication.java
@@ -16,6 +16,6 @@ public class AzuresampleappApplication {
     @GetMapping("/")
     public String display(){
         
-        return "Hello User!!!!";
+        return "Hello Staging User!!!!";
     }
 }

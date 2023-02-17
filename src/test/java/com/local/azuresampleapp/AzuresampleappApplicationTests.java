07527088ad8bdf23d285518f9dc83f4e5c1e0b39
kmohan770 <kmohna770@gmail.com>
2023-02-17 13:55:43 +0530

    sample app

---
+++ src/test/java/com/local/azuresampleapp/AzuresampleappApplicationTests.java
@@ -0,0 +1,15 @@
+package com.local.azuresampleapp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuresampleappApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
